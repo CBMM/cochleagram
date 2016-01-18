10882bd14ca624d6cf5d7b41daa2991632a7dfa5
Greg Hale <imalsogreg@gmail.com>
2016-01-18 17:52:39 -0500

    un-gray

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 ./static.sh
-cp static/* dist/build/cochleagram/cochleagram.jsexe/
+cp ./static/* ./dist/build/cochleagram/cochleagram.jsexe/
 cd dist/build/cochleagram/cochleagram.jsexe && git add *.js *.stats *.css *.html && git commit -m "build" && git push origin gh-pages
